33f4b8c72ff22db150de59b5bd391500b43748b9
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-08-16 17:12:17 -0300

    adicionando modal

--- js/reservas.js
+++ js/reservas.js
@@ -1,2 +1,15 @@
+const iniciaModal = (modalID) => {
+    const modal = document.querySelector(modalID)
+    modal.classList.add('mostrar')
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar')
+        modal.classList.remove('mostrar')
+    })
+}
 
+
+const botao = document.querySelectorAll('.btn')
+for (let i = 0; i < botao.length; i++) {
+    botao[i].addEventListener('click', () => iniciaModal('.reserva-modal'))
+}
 
