e8b11d01e28732597a4a16607a63f565e920aece
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-08-17 10:32:52 -0300

    adicionando melhorias no index

--- js/index.js
+++ js/index.js
@@ -50,3 +50,30 @@ cadReserva.addEventListener('submit', async (event) =>{
     alert("Ingresso reservado com sucesso!")
 
 })
+
+// função para mostrar os três primeiros eventos cadastrados
+
+const urlCad = 'https://xp41-soundgarden-api.herokuapp.com/events'
+
+const exibirEventos = async () => {
+    const resposta = await fetch(urlCad);
+    const data = await resposta.json();
+    const cad = data.slice(0,3)
+
+    const card = document.querySelector('#primeiros');
+    let htmlEventos = ""
+    cad.forEach((event) => {
+
+        htmlEventos += `
+        <article class="evento card p-5 m-3">
+        <h2>${event.name} - ${event.scheduled}</h2>
+        <h4>${event.attractions}</h4>
+        <p>${event.description}</p>
+        <a href="#" class="btn btn-primary">reservar ingresso</a>
+        </article>
+        `
+});
+   card.innerHTML = htmlEventos
+};
+
+exibirEventos()
